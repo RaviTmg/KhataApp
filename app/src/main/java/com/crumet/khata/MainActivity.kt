24325a6300b444aa60b67b4b2075c355fc7597f1
RaviTmg <tmgrawv@gmail.com>
2018-08-08 10:10:41 +0545

    added linechart, changed ui

--- app/src/main/java/com/crumet/khata/MainActivity.kt
+++ app/src/main/java/com/crumet/khata/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(bottom_bar)
-        startActivity(Intent(this, LoginActivity::class.java))
         fab.setOnClickListener {
             startActivity(Intent(this, AddRecordActivity::class.java))
 
